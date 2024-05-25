0560faff5ce623c9d89a620016860fbd374064fc
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-25 17:24:40 +0200

    mlflow is added in trainer

--- src/trainer.py
+++ src/trainer.py
@@ -1,12 +1,20 @@
 import os
 import torch
+import mlflow
 import argparse
 import numpy as np
 from tqdm import tqdm
 from utils import config, dump, load
 from helper import helpers
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    confusion_matrix,
+)
 
 
 class Trainer:
@@ -32,6 +40,8 @@ class Trainer:
         self.device = device
         self.is_display = is_display
 
+        self.experiment_name = "Breast Cancer Classification"
+
         self.init = helpers(
             lr=self.lr,
             beta1=self.beta1,
@@ -52,6 +62,8 @@ class Trainer:
         self.loss = float("inf")
         self.config = config()
 
+        self.raw_data_path = self.config["path"]["raw_path"]
+        self.files_path = self.config["path"]["files_path"]
         self.train_models_path = self.config["path"]["train_models_path"]
         self.best_model_path = self.config["path"]["best_model_path"]
 
@@ -71,37 +83,22 @@ class Trainer:
         else:
             raise TypeError("X and y must be torch.Tensor")
 
-    def display_progress(
-        self,
-        epoch,
-        train_loss,
-        test_loss,
-        train_pred,
-        train_actual,
-        valid_pred,
-        valid_actual,
-    ):
-
-        train_pred_flat = np.concatenate(train_pred)
-        train_actual_flat = np.concatenate(train_actual)
-        valid_pred_flat = np.concatenate(valid_pred)
-        valid_actual_flat = np.concatenate(valid_actual)
-
+    def display_progress(self, **kwargs):
         if self.is_display:
-
             print(
                 "Epochs - [{}/{}] - Train Loss: {:.4f} - Test Loss: {:.4f} - train_accuracy: {:.4f} - test_accuracy: {:.4f}".format(
-                    epoch + 1,
+                    kwargs["epoch"] + 1,
                     self.epochs,
-                    np.mean(train_loss),
-                    np.mean(test_loss),
-                    accuracy_score(train_actual_flat, train_pred_flat),
-                    accuracy_score(valid_actual_flat, valid_pred_flat),
+                    np.mean(kwargs["train_loss"]),
+                    np.mean(kwargs["test_loss"]),
+                    accuracy_score(kwargs["train_actual"], kwargs["train_pred"]),
+                    accuracy_score(kwargs["valid_actual"], kwargs["valid_pred"]),
                 )
             )
-
         else:
-            print("Epochs - [{}/{}] is completed".format(epoch + 1, self.epochs))
+            print(
+                "Epochs - [{}/{}] is completed".format(kwargs["epoch"] + 1, self.epochs)
+            )
 
     def saved_models(self, epoch=None, train_loss=None):
         torch.save(
@@ -110,7 +107,6 @@ class Trainer:
         )
 
         if train_loss is not None:
-
             if self.loss > train_loss:
                 self.loss = train_loss
                 torch.save(
@@ -121,63 +117,122 @@ class Trainer:
                     },
                     os.path.join(self.best_model_path, "best_model.pth"),
                 )
-
         else:
             raise ValueError("Train loss cannot be None".capitalize())
 
     def train(self):
-        for epoch in tqdm(range(self.epochs)):
-            train_predcit = []
-            valid_predict = []
-            train_actual = []
-            valid_actual = []
-            train_loss = []
-            test_loss = []
+        mlflow.set_experiment(self.experiment_name)
 
-            for _, (X, y) in enumerate(self.train_dataloader):
-                X, y = X.to(self.device), y.to(self.device)
-                predicted, loss = self.update_model(X=X, y=y.unsqueeze(1).float())
+        with mlflow.start_run(
+            description="This is used for to track the Breast Cancer Classification Model loss"
+        ) as run:
+            for epoch in tqdm(range(self.epochs)):
+                train_predcit = []
+                valid_predict = []
+                train_actual = []
+                valid_actual = []
+                train_loss = []
+                test_loss = []
 
-                train_predcit.append(
-                    torch.where(predicted > 0.5, 1, 0).detach().cpu().numpy()
+                for _, (X, y) in enumerate(self.train_dataloader):
+                    X, y = X.to(self.device), y.to(self.device)
+                    predicted, loss = self.update_model(X=X, y=y.unsqueeze(1).float())
+
+                    train_predcit.append(
+                        torch.where(predicted > 0.5, 1, 0).detach().cpu().numpy()
+                    )
+                    train_actual.append(
+                        torch.where(y > 0.5, 1, 0).detach().cpu().numpy()
+                    )
+
+                    train_loss.append(loss)
+
+                for _, (X, y) in enumerate(self.valid_dataloader):
+                    X, y = X.to(self.device), y.to(self.device)
+                    predicted = self.model(X)
+                    loss = self.criterion(predicted, y.unsqueeze(1).float())
+
+                    valid_predict.append(
+                        torch.where(predicted > 0.5, 1, 0).detach().cpu().numpy()
+                    )
+                    valid_actual.append(
+                        torch.where(y > 0.5, 1, 0).detach().cpu().numpy()
+                    )
+
+                    test_loss.append(loss.item())
+
+                train_actual = np.concatenate(train_actual)
+                train_predcit = np.concatenate(train_predcit)
+                valid_actual = np.concatenate(valid_actual)
+                valid_predict = np.concatenate(valid_predict)
+
+                self.display_progress(
+                    epoch=epoch,
+                    train_loss=train_loss,
+                    test_loss=test_loss,
+                    train_pred=train_predcit,
+                    train_actual=train_actual,
+                    valid_pred=valid_predict,
+                    valid_actual=valid_actual,
                 )
-                train_actual.append(torch.where(y > 0.5, 1, 0).detach().cpu().numpy())
 
-                train_loss.append(loss)
+                self.saved_models(epoch=epoch + 1, train_loss=np.mean(train_loss))
 
-            for _, (X, y) in enumerate(self.valid_dataloader):
-                X, y = X.to(self.device), y.to(self.device)
-                predicted = self.model(X)
-                loss = self.criterion(predicted, y.unsqueeze(1).float())
+                self.history["train_loss"].append(np.mean(train_loss))
+                self.history["test_loss"].append(np.mean(test_loss))
 
-                valid_predict.append(
-                    torch.where(predicted > 0.5, 1, 0).detach().cpu().numpy()
-                )
-                valid_actual.append(torch.where(y > 0.5, 1, 0).detach().cpu().numpy())
+                train_accuracy = accuracy_score(train_actual, train_predcit)
+                valid_accuracy = accuracy_score(valid_actual, valid_predict)
+                train_precision = precision_score(train_actual, train_predcit)
+                valid_precision = precision_score(valid_actual, valid_predict)
+                train_recall = recall_score(train_actual, train_predcit)
+                valid_recall = recall_score(valid_actual, valid_predict)
 
-                test_loss.append(loss.item())
+                mlflow.log_metric("train_loss", np.mean(train_loss), step=epoch + 1)
+                mlflow.log_metric("test_loss", np.mean(test_loss), step=epoch + 1)
 
-            self.display_progress(
-                epoch,
-                train_loss,
-                test_loss,
-                train_predcit,
-                train_actual,
-                valid_predict,
-                valid_actual,
+                mlflow.log_metric("train_accuracy", train_accuracy, step=epoch + 1)
+                mlflow.log_metric("valid_accuracy", valid_accuracy, step=epoch + 1)
+
+                mlflow.log_metric("train_precision", train_precision, step=epoch + 1)
+                mlflow.log_metric("valid_precision", valid_precision, step=epoch + 1)
+
+                mlflow.log_metric("train_recall", train_recall, step=epoch + 1)
+                mlflow.log_metric("valid_recall", valid_recall, step=epoch + 1)
+
+            print("Training is completed".title())
+
+            dump(
+                value=self.history,
+                filename=os.path.join(config()["path"]["files_path"], "history.pkl"),
             )
 
-            self.saved_models(epoch=epoch + 1, train_loss=np.mean(train_loss))
+            mlflow.log_params(
+                {
+                    "epochs": self.epochs,
+                    "lr": self.lr,
+                    "beta1": self.beta1,
+                    "beta2": self.beta2,
+                    "adam": self.adam,
+                    "SGD": self.SGD,
+                    "momentun": self.momentum,
+                    "device": self.device,
+                    "display": self.is_display,
+                }
+            )
 
-            self.history["train_loss"].append(np.mean(train_loss))
-            self.history["test_loss"].append(np.mean(test_loss))
+            mlflow.pytorch.log_model(self.model, "Breast_Cancer_Model")
 
-        print("Training is completed")
+            mlflow.log_artifact(
+                os.path.join(self.files_path, "Model.jpeg"),
+            )
 
-        dump(
-            value=self.history,
-            filename=os.path.join(config()["path"]["files_path"], "history.pkl"),
-        )
+            mlflow.log_artifacts(
+                os.path.join(
+                    self.raw_data_path,
+                ),
+                artifact_path="dataset",
+            )
 
     @staticmethod
     def plot_history():
@@ -194,6 +249,7 @@ class Trainer:
             plt.legend()
 
         plt.tight_layout
+        plt.savefig(os.path.join(config()["path"]["files_path"], "loss.png"))
         plt.show()
 
 
